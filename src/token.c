3119f13446bf174c183518e141fe6706d17ed807
Cathal O'Grady <fierceinbattle@gmail.com>
2023-03-15 20:09:26 +0000

    added declaration of plagiarism policy

--- src/token.c
+++ src/token.c
@@ -1,12 +1,41 @@
+/*
+Cathal O'Grady, 21442084 - Operating Systems Project 1
+ca216/myshell
+
+I understand that the University regards breaches of academic integrity and
+plagiarism as grave and serious. I have read and understood the DCU Academic
+Integrity and Plagiarism Policy. I accept the penalties that may be imposed
+should I engage in practice or practices that breach this policy. I have
+identified and included the source of all facts, ideas, opinions and
+viewpoints of others in the assignment references. Direct quotations,
+paraphrasing, discussion of ideas from books, journal articles, internet
+sources, module text, or any other source whatsoever are acknowledged and the
+sources cited are identified in the assignment references. I declare that
+this material, which I now submit for assessment, is entirely my own work and
+has not been taken from the work of others save and to the extent that such
+work has been cited and acknowledged within the text of my work. I have used
+the DCU library referencing guidelines (available at
+https://www4.dcu.ie/library/classes_and_tutorials/citingreferencing.shtml
+and/or the appropriate referencing system recommended in the assignment
+guidelines and/or programme documentation. By signing this form or by
+submitting material online I confirm that this assignment, or any part of it,
+has not been previously submitted by me or any other person for assessment on
+this or any other course of study By signing this form or by submitting
+material for assessment online I confirm that I have read and understood the
+DCU Academic Integrity and Plagiarism Policy (available at
+http://www.dcu.ie/registry/examinations/index.shtml).
+*/
+
+#include <stdint.h>
+
+#include "token.h"
+
 /* This file implements a token matcher. The token matcher works by
  * matching characters against columns of each items token field in a queryset
  * of type tok_map_t. This is used for commandline parsing in parser.c, it
  * allows you to map a token to a function.
  */
 
-#include <stdint.h>
-
-#include "token.h"
 
 /**
  * Part of the token matcher, will be called multiple times the caller will
